2d2ae51016ebacc1f93cb5ceb9635401c408a77d
Kei-K23 <keiksl2301@gmail.com>
2024-06-22 09:01:08 +0630

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BSN (Book Social Network)
+
+!!! BSN is under development
---
+++ book-network-api/src/test/java/com/dev/kei/book/network/api/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.kei.book.network.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
