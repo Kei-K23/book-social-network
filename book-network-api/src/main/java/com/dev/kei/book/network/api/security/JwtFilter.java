c6906a24d7e28e1ed8620457fce357660387993c
Kei-K23 <keiksl2301@gmail.com>
2024-06-22 12:05:41 +0630

    API:Add jwt filter

--- book-network-api/src/main/java/com/dev/kei/book/network/api/security/JwtFilter.java
+++ book-network-api/src/main/java/com/dev/kei/book/network/api/security/JwtFilter.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -52,6 +53,5 @@ public class JwtFilter extends OncePerRequestFilter {
             }
         }
         filterChain.doFilter(request, response);
-
     }
 }
