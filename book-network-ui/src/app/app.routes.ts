ad50ed76fff36f881f20e708111181ac33a5f487
Kei-K23 <keiksl2301@gmail.com>
2024-07-05 22:05:31 +0630

    UI: add login logic with request to backend api

--- book-network-ui/src/app/app.routes.ts
+++ book-network-ui/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
+import {RegisterComponent} from "./pages/register/register.component";
 
 export const routes: Routes = [
   {
     path: "login",
     component: LoginComponent
-  }
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
 ];
--- book-network-ui/src/app/pages/login/login.component.ts
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import {AuthLoginRequest} from "../../services/models/auth-login-request";
 import {FormsModule} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {NgForOf, NgIf} from "@angular/common";
+import {TokenService} from "../../services/token/token.service";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgIf,
+    NgForOf
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -15,5 +21,40 @@ export class LoginComponent {
   authRequest : AuthLoginRequest = {email: "", password: ""};
   errorMsg: Array<string> = [];
 
+  constructor(
+    private router : Router,
+    private authService : AuthenticationService,
+    private tokenService: TokenService
+  ) {
+  }
+
+  login() {
+    // Make sure to clear the errorMsg array
+    this.errorMsg = [];
+    this.authService.login(
+      {
+        body: this.authRequest
+      }
+    ).subscribe({
+      next: res => {
+       // Save the JWT token in local storage
+        this.tokenService.token = res.token as string;
+       // Navigate to main screen
+       this.router.navigate(['books']);
+      },
+      error: err => {
+        console.log(err)
+        if (err.error.validationErrors) {
+          this.errorMsg = err.error.validationErrors;
+        } else {
+          this.errorMsg.push(err.error.error);
+        }
+      }
+    })
+  }
+
+  register() {
+    this.router.navigate(['register']);
+  }
 
 }
