e18bef971e2afa27e0b71ec0c26150663c3aeee7
Kei-K23 <keiksl2301@gmail.com>
2024-07-13 17:59:26 +0630

    Refactor the style for book card

--- book-network-ui/src/app/module/book/components/menu/menu.component.ts
+++ book-network-ui/src/app/module/book/components/menu/menu.component.ts
@@ -3,29 +3,45 @@ import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 import {LocalStorageService} from "../../../../services/localStorage/local-storage.service";
 import {KEYS} from "../../../../constants/keys";
 import {JwtTokenService} from "../../../../services/jwt-token/jwt-token.service";
+import {UsersService} from "../../../../services/services/users.service";
+import {UserResponse} from "../../../../services/models/user-response";
+import {ToastrService} from "ngx-toastr";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    NgIf
   ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent implements OnInit {
-  username: string = "";
+  userResponse: UserResponse = {};
 
   constructor(
     private localStorageService : LocalStorageService,
+    private usersService: UsersService,
     private router : Router,
+    private toastr: ToastrService,
     private jwtTokenService : JwtTokenService
   ) {
   }
 
   ngOnInit() {
-    this.username = this.jwtTokenService.getValueFromJwt("fullName") || "";
+    this.getAuthUserData();
+  }
+
+  getAuthUserData() {
+    this.usersService.getLoginUser().subscribe({
+      next: value => {
+        this.userResponse = value;
+      },
+      error: err => this.toastr.error(err.error.error)
+    });
   }
 
   onLogout() {
